a3146a25cf8adeb1b1bb5c45d538f72d01dc35ef
ariesduanmu <aries.duanmu@gmail.com>
2018-10-11 22:56:08 +0800

    applyRules for each bird in different size

--- dungeon/bird_room.py
+++ dungeon/bird_room.py
@@ -56,27 +56,35 @@ the pdist and squareform is really anamzing, scipy is amazing
 class Birds():
     def __init__(self, n, width, height):
         self.pos = [width/2, height/2] + 10 * np.random.rand(2*n).reshape(n, 2)
+        # width/height = 5-10
+        self.size = ([15, 15] + 5 * np.random.rand(2*n).reshape(n, 2))*2
+        self.minDistanceMatrix = squareform(pdist(self.size, lambda u, v: sum(np.hypot([u[0],v[0]],[u[1],v[1]]))/2))
+        # pprint(self.minDistanceMatrix)
         angles = 2*math.pi*np.random.rand(n)
         self.vel = np.array(list(zip(np.sin(angles), np.cos(angles))))
+        # self.vel = np.zeros(2*n).reshape(n, 2)
         self.n = n
-        self.minDist = 25.0
         self.maxRuleVel = 0.03
         self.maxVel = 2.0
         self.width = width
         self.height = height
+        self.stop = False
 
     def tick(self, frameNum, pts, beak):
-        distMatrix_x = squareform(pdist(self.pos, lambda u, v: abs(u-v)[0]))
-        distMatrix_y = squareform(pdist(self.pos, lambda u, v: abs(u-v)[1]))
-        self.distMatrix = np.vstack((distMatrix_x.reshape(1,self.n*self.n),\
-                                     distMatrix_y.reshape(1,self.n*self.n))).T\
-                                    .reshape(self.n, self.n, 2)
+        # it is slow and not a good way
+        # distMatrix_x = squareform(pdist(self.pos, lambda u, v: abs(u-v)[0]))
+        # distMatrix_y = squareform(pdist(self.pos, lambda u, v: abs(u-v)[1]))
+        # self.distMatrix = np.vstack((distMatrix_x.reshape(1,self.n*self.n),\
+        #                              distMatrix_y.reshape(1,self.n*self.n))).T\
+        #                             .reshape(self.n, self.n, 2)
+        self.distMatrix = squareform(pdist(self.pos))
         self.vel += self.applyRules()
         self.limit(self.vel, self.maxVel)
         self.pos += self.vel
-        # self.applyBC()
-
-        # self.vel = self.vel*0.9
+        if not self.stop:
+            self.applyBC()
+        if self.stop:
+            self.vel = self.vel*0.9
         pts.set_data(self.pos.reshape(2*self.n)[::2],
                      self.pos.reshape(2*self.n)[1::2])
         vec = self.pos + 10*self.vel/self.maxVel
@@ -95,38 +103,17 @@ class Birds():
 
     def applyBC(self):
         for coord in self.pos:
-            if coord[0] > self.width:
+            if coord[0] > self.width or coord[0] < 0 or coord[1] > self.height or coord[1] < 0:
                 self.stop = True
-                coord[0] = self.width
-            if coord[0] < 0:
-                self.stop = True
-                coord[0] = 0
-            if coord[1] > self.height:
-                self.stop = True
-                coord[1] = self.height
-            if coord[1] < 0:
-                self.stop = True
-                coord[1] = 0
+                break
 
     def applyRules(self):
-        D = self.distMatrix < 2.0
-        D = D.prod(axis=2)
+        D = self.distMatrix < self.minDistanceMatrix
+        pprint((D.sum(axis=1)).sum(axis=0))
         vel = self.pos*D.sum(axis=1).reshape(self.n, 1) - D.dot(self.pos)
         self.limit(vel, self.maxRuleVel)
 
         return vel
-
-    def buttonPress(self, event):
-        # left click - add a bird
-        if event.button == 1:
-            self.pos = np.concatenate((self.pos, np.array([[event.xdata, event.ydata]])), axis=0)
-            angles = 2*math.pi*np.random.rand(1)
-            v = np.array(list(zip(np.sin(angles), np.cos(angles))))
-            self.vel = np.concatenate((self.vel, v), axis=0)
-            self.n += 1
-        # right click - scatter
-        elif event.button == 3:
-            self.vel += 0.1*(self.pos - np.array([[event.xdata, event.ydata]]))
 
 
 def tick(frameNum, pts, beak, birds):
@@ -151,8 +138,6 @@ def main():
     pts, = ax.plot([], [], markersize=10, color='b', marker='o', ls='None')
     beak, = ax.plot([], [], markersize=4, color='c', marker='o', ls='None')
     anim = animation.FuncAnimation(fig, func=tick, fargs=(pts, beak, birds), interval=50)
-
-    cid = fig.canvas.mpl_connect('button_press_event', birds.buttonPress)
     plt.show()
 
 
